5fa63c5cc03226c8e8471a9aecf5fd3693c3c47c
trebonius0 <trebonius@worldofwargraphs.com>
2017-01-10 19:27:55 +0100

    Routes cannot be changed

--- src/com/trebonius/phototo/Routes.java
+++ src/com/trebonius/phototo/Routes.java
@@ -2,7 +2,7 @@ package com.trebonius.phototo;
 
 public class Routes {
 
-    public static String thumbnailRootUrl = "/img/thumbnail";
-    public static String fullSizePicturesRootUrl = "/img/fullsize";
-    public static String listItemsApiUrl = "/api/list";
+    public static final String thumbnailRootUrl = "/img/thumbnail";
+    public static final String fullSizePicturesRootUrl = "/img/fullsize";
+    public static final String listItemsApiUrl = "/api/list";
 }
