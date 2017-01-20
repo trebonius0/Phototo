4c0c58e02b3bcdbdb0cbd7000de4e891ee060c03
trebonius0 <trebonius@worldofwargraphs.com>
2017-01-20 11:54:22 +0100

    Md5 fix + unit tests

--- src/phototo/helpers/Md5.java
+++ src/phototo/helpers/Md5.java
@@ -1,18 +1,23 @@
 package phototo.helpers;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public final class Md5 {
 
     public static String encodeString(String str) {
-        byte[] uniqueKey = str.getBytes();
+        if (str == null) {
+            throw new IllegalArgumentException();
+        }
         byte[] hash = null;
 
         try {
-            hash = MessageDigest.getInstance("MD5").digest(uniqueKey);
+            hash = MessageDigest.getInstance("MD5").digest(str.getBytes("UTF8"));
         } catch (NoSuchAlgorithmException e) {
             throw new Error("No MD5 support in this VM.");
+        } catch (UnsupportedEncodingException e) {
+            throw new Error("Unsupported encoding");
         }
 
         StringBuilder hashString = new StringBuilder(40);
