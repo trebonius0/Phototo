0a3ac8b9a0e71e628bb7024bbd8087f6657b657a
trebonius0 <trebonius@worldofwargraphs.com>
2017-02-14 21:20:00 +0100

    Typo fix

--- www/js/ts/messages.ts
+++ www/js/ts/messages.ts
@@ -1,4 +1,4 @@
 class Messages {
-    public static serverError: string = "A server error has been encoutered. Please retry later."
+    public static serverError: string = "A server error has been encountered. Please retry later."
     public static noResult: string = "No result";
 }
